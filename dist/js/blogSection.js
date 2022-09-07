7fa5e8c176d1c60271b8b0b0e2f68b3cfcc5f264
bunq <101729745+bunq-website@users.noreply.github.com>
2022-09-07 14:08:07 +0200

    Update blogSection.js

--- dist/js/blogSection.js
+++ dist/js/blogSection.js
@@ -1,1 +1,1 @@
-"use strict";(()=>{var h=$(".blog-4_mask").height();$(".blog-cards_item").each(function(){$(".w-dyn-item").height(h)});})();
+"use strict";(()=>{var e=$(".blog-4_mask").height();$(".blog-cards_item").each(function(){$(".w-dyn-item .blog-cards_item").height(e)});})();
