fb622b86f85692bd8c3df3e901b255a223673360
bunq <101729745+bunq-website@users.noreply.github.com>
2022-10-20 11:51:24 +0200

    Removed if statement benefitPage

--- src/js/benefitPage.js
+++ src/js/benefitPage.js
@@ -6,40 +6,37 @@ $(document).ready(function () {
 });
 
 // If video is present, manage autoplay and autopause when entering and exiting viewport
-let iframe = document.getElementById('iframe').getAttribute('src');
-if (iframe !== '') {
-  let video = document.getElementById('video'),
-    fraction = 0.8;
+let video = document.getElementById('video'),
+  fraction = 0.8;
 
-  function checkScroll() {
-    let x = video.offsetLeft,
-      y = video.offsetTop,
-      w = video.offsetWidth,
-      h = video.offsetHeight,
-      r = x + w, //right
-      b = y + h, //bottom
-      visibleX,
-      visibleY,
-      visible;
+function checkScroll() {
+  let x = video.offsetLeft,
+    y = video.offsetTop,
+    w = video.offsetWidth,
+    h = video.offsetHeight,
+    r = x + w, //right
+    b = y + h, //bottom
+    visibleX,
+    visibleY,
+    visible;
 
-    visibleX = Math.max(
-      0,
-      Math.min(w, window.pageXOffset + window.innerWidth - x, r - window.pageXOffset)
-    );
-    visibleY = Math.max(
-      0,
-      Math.min(h, window.pageYOffset + window.innerHeight - y, b - window.pageYOffset)
-    );
+  visibleX = Math.max(
+    0,
+    Math.min(w, window.pageXOffset + window.innerWidth - x, r - window.pageXOffset)
+  );
+  visibleY = Math.max(
+    0,
+    Math.min(h, window.pageYOffset + window.innerHeight - y, b - window.pageYOffset)
+  );
 
-    visible = (visibleX * visibleY) / (w * h);
+  visible = (visibleX * visibleY) / (w * h);
 
-    if (visible > fraction) {
-      video.play();
-    } else {
-      video.pause();
-    }
+  if (visible > fraction) {
+    video.play();
+  } else {
+    video.pause();
   }
-  checkScroll();
-  window.addEventListener('scroll', checkScroll, false);
-  window.addEventListener('resize', checkScroll, false);
 }
+checkScroll();
+window.addEventListener('scroll', checkScroll, false);
+window.addEventListener('resize', checkScroll, false);
